715f303bf30c86bcd3628e82fc7731ce0addfdd3
kaushikraghupathruni <kaushikraghupathruni@gmail.com>
2020-05-15 15:35:51 +0530

    refactor commit

--- print_board.py
+++ print_board.py
@@ -5,9 +5,15 @@ import numpy as np
 class PrintBoard:
     
     def print_board(self, grid):
-        
+        """
+        print sudoku board
+
+        params: 
+        grid - numpy array of (9,9)
+        """
         if not self.__is_valid_grid(grid):
             raise ValueError("input grid is not valid")
+
         print("+" + "---+"*9)
         for i, row in enumerate(grid):
             print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
@@ -19,6 +25,10 @@ class PrintBoard:
     def __is_valid_grid(self, grid):
         """
         Check if the list has 9 nested lists, each should have 9 elements 
+
+        params: 
+        grid - numpy array of (9,9)
+        
         return: Boolean value
         """
         if grid.shape == (9,9):
